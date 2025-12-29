3ad29a6f5978fa36b3e2348b2c7c37f5b00524e7
Pratik Tarkasband <tpratik345@gmail.com>
2025-12-29 21:55:59 +0530

    Added overlap logic

--- React/draggable-notes-app/src/components/Notes.jsx
+++ React/draggable-notes-app/src/components/Notes.jsx
@@ -4,7 +4,7 @@ import React, { createRef, useEffect, useRef, useState } from 'react'
 function Notes() {
     const inputRef = useRef();
     const notesRef = useRef([]);
-    const [counter, setCounter] = useState(1);
+    const [counter, setCounter] = useState(localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes'))?.length+1 : 1);
     const [notes, setNotes] = useState(localStorage.getItem('notes') ? JSON.parse(localStorage.getItem('notes')) : []);
 
     function getPosition() {
@@ -51,6 +51,8 @@ function Notes() {
         const offsetX = e.clientX - rect.left;
         const offsetY = e.clientY - rect.top;
 
+        const startPosition = note.position;
+
         // It will determine the position of the not when we are dragging that
         const handleMouseMove = (event) => {
             const newX = event.clientX - offsetX;
@@ -58,10 +60,12 @@ function Notes() {
 
             noteRef.style.left = `${newX}px`;
             noteRef.style.top = `${newY}px`;
+            noteRef.style.cursor = 'grabbing';
         }
 
         //It will check, when we leave note after the draging it has to stay in particular position
         const handleMouseUp = () => {
+            noteRef.style.cursor = 'move';
             document.removeEventListener('mousemove', handleMouseMove);
             document.removeEventListener('mouseup', handleMouseUp);
 
@@ -69,8 +73,9 @@ function Notes() {
             const newPosition = { x: finalRect.left, y: finalRect.top };
 
             // Check for overlap
-            if(false) {
-
+            if(checkOverlap(note.id)) {
+                noteRef.style.left = `${startPosition.x}px`;
+                noteRef.style.top = `${startPosition.y}px`;
             } else {
                 const updatedNote = notes.map((mapNote) => mapNote.id===note.id ? {...mapNote, position: newPosition} : mapNote);
                 setNotes(updatedNote);
@@ -82,8 +87,25 @@ function Notes() {
         document.addEventListener('mouseup', handleMouseUp);
     }
 
-    // TODO: below function
-    function checkOverlap() {
+    function checkOverlap(id) {
+        const currentNoteRef = notesRef.current[id].current;
+        const currentNoteRect = currentNoteRef.getBoundingClientRect();
+
+        return notes.some((mapNote) => {
+            if(mapNote.id===id) return false;
+
+            let otherNoteRef = notesRef.current[mapNote.id].current;
+            let otherNoteRect = otherNoteRef.getBoundingClientRect();
+
+            let overlap = !(
+                currentNoteRect.right < otherNoteRect.left ||
+                currentNoteRect.left > otherNoteRect.right ||
+                currentNoteRect.button < otherNoteRect.top ||
+                currentNoteRect.top > otherNoteRect.bottom
+            )
+
+            return overlap;
+        })
 
     }
 
@@ -103,28 +125,29 @@ function Notes() {
             </div>
             <div className='notes'>
                 {notes && notes?.map((note) => {
-                    return (<div
-                        style={{
-                            backgroundColor: '#F5B027',
-                            color: 'black',
-                            border: '2px solid black',
-                            position: 'absolute',
-                            left: `${note.position.x}px`,
-                            top: `${note.position.y}px`,
-                            userSelect: 'none',
-                            padding: '10px',
-                            width: '200px',
-                            cursor: 'move'
-                        }}
-                        ref={
-                            notesRef.current[note.id]
-                                ? notesRef.current[note.id]
-                                : (notesRef.current[note.id] = createRef())}
-                        onMouseDown={(e) => handleDragStart(e, note)}
-                        key={note.id}
-                        className={`note-${note.id}`}>
-                        📌 {note.text}
-                    </div>)
+                    return (
+                        <div
+                            style={{
+                                backgroundColor: '#e8c173ff',
+                                color: 'black',
+                                border: '2px solid black',
+                                position: 'absolute',
+                                left: `${note.position.x}px`,
+                                top: `${note.position.y}px`,
+                                userSelect: 'none',
+                                padding: '10px',
+                                width: '200px',
+                                cursor: 'move'
+                            }}
+                            ref={
+                                notesRef.current[note.id]
+                                    ? notesRef.current[note.id]
+                                    : (notesRef.current[note.id] = createRef())}
+                            onMouseDown={(e) => handleDragStart(e, note)}
+                            key={note.id}
+                            className={`note-${note.id}`}>
+                            📌 {note.text}
+                        </div>)
                 })}
             </div>
         </div>
