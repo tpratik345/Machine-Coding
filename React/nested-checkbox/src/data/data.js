d2af7bf394aa8b602331b962bf422d152ce9a7a1
Pratik Tarkasband <tpratik345@gmail.com>
2026-03-01 21:50:21 +0530

    Added data

---
+++ React/nested-checkbox/src/data/data.js
@@ -0,0 +1,36 @@
+export const checkBoxData = [
+    {
+        id: 1,
+        name: 'Fruits',
+        children: [
+            {
+                id: 2,
+                name: 'Citrus',
+                children: [
+                    {
+                        id: 3,
+                        name: 'Orange'
+                    },
+                    {
+                        id: 4,
+                        name: 'Lemon'
+                    },
+                ]
+            },
+            {
+                id: 5,
+                name: 'Berries',
+                children: [
+                    {
+                        id: 6,
+                        name: 'Strawberry'
+                    },
+                    {
+                        id: 7,
+                        name: 'Blueberry'
+                    }
+                ]
+            }
+        ]
+    }
+]
