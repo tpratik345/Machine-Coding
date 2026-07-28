c46de9d00b3e33720e664259fecd5bacb792849e
tpratik345 <tpratik345@gmail.com>
2026-07-28 18:10:08 +0530

    Updated

--- React/star-ratings-half/src/App.jsx
+++ React/star-ratings-half/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import StarRatings from './components/StarRatings'
+import './App.css'
 
 function App() {
 
@@ -11,9 +9,9 @@ function App() {
   }
 
   return (
-    <>
-      <StarRatings size={10} handleSelectedRatings={handleSelectedRatings}/>
-    </>
+    <div id='center'>
+      <StarRatings size={5} handleSelectedRatings={handleSelectedRatings}/>
+    </div>
   )
 }
 
--- React/star-ratings-half/src/components/StarRatings.jsx
+++ React/star-ratings-half/src/components/StarRatings.jsx
@@ -1,41 +1,80 @@
-import { useState } from 'react'
-import './StarRatings.css'
+import { useState } from "react";
+import "./StarRatings.css";
+import { AiFillStar } from "react-icons/ai";
 
 function StarRatings({ size, handleSelectedRatings }) {
   const [starsSelected, setSelectedStars] = useState(0);
   const [hoveredRatings, setHoveredRatings] = useState(0);
-  
-  function handleClick(e) {
-    console.log(e.getBoundingClientRect())
+
+  function handleClick(e, starValue) {
+    const { clientX, clientY } = e;
+    const { left, right } = e.target.getBoundingClientRect();
+    const halfPosition = (right - left) / 2 + left;
+    // console.log({ clientX, clientY })
+    // console.log({ left, right })
+    // console.log(halfPosition)
+
+    if (clientX < halfPosition) {
+      starValue -= 0.5;
+    }
+
     setSelectedStars(starValue);
-    // handleSelectedRatings(starValue)
+    handleSelectedRatings(starValue)
   }
-  
+
+  function handleMouseEnter(e, value) {
+    const { clientX, clientY } = e;
+    const { left, right } = e.target.getBoundingClientRect();
+    const halfPosition = (right - left) / 2 + left;
+    // console.log({ clientX, clientY })
+    // console.log({ left, right })
+    // console.log(halfPosition)
+
+    if (clientX < halfPosition) {
+      value -= 0.5;
+    }
+    setHoveredRatings(value);
+  }
+
   return (
-    <div className='container'>
+    <div className="container">
       {[...Array(size)].map((_, index) => {
         const starValue = index + 1;
 
-        let starClass = 'star';
+        const currentRating = hoveredRatings || starsSelected;
 
-        if (starValue <= hoveredRatings) {
-          starClass += ' hover'
-        } else if (starValue <= starsSelected) {
-          starClass += ' active'
+        let fillPercentage = 0;
+
+        if (currentRating >= starValue) {
+          fillPercentage = 100;
+        } else if (currentRating >= starValue - 0.5) {
+          fillPercentage = 50;
+        } else {
+          fillPercentage = 0;
         }
 
-        return <span
-          className={starClass}
-          key={index}
-          onClick={handleClick}
-          onMouseEnter={() => setHoveredRatings(starValue)}
-          onMouseLeave={() => setHoveredRatings(0)}
-        >&#9733;</span>
-      })
-
-      }
+        return (
+          <span
+            className="star-wrapper"
+            key={index}
+            onClick={(e) => handleClick(e, starValue)}
+            onMouseEnter={(e) => handleMouseEnter(e, starValue)}
+            onMouseLeave={() => setHoveredRatings(0)}
+          >
+            <AiFillStar className="empty-star" />
+            {index <= currentRating - 0.5 && (
+              <span
+                className="filled-star"
+                style={{ width: `${fillPercentage}%` }}
+              >
+                <AiFillStar />
+              </span>
+            )}
+          </span>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default StarRatings
+export default StarRatings;
