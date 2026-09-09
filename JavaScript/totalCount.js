3429abf803c7485b2f2ce5a4aea4dfbf2b4718ec
tpratik345 <tpratik345@gmail.com>
2026-09-09 12:02:29 +0530

    Added max count

--- JavaScript/totalCount.js
+++ JavaScript/totalCount.js
@@ -1,6 +1,7 @@
-let a = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple'];
+// get the max count element
+let a = ['apple', 'banana', 'apple', 'orange', 'banana', 'apple', 'banana', 'banana'];
 
-let result = a.reduce((acc, curr) => {
+let count = a.reduce((acc, curr) => {
     if(!acc[curr]) acc[curr] = 0;
 
     acc[curr] += 1;
@@ -8,4 +9,11 @@ let result = a.reduce((acc, curr) => {
     return acc;
 }, {})
 
-console.log(result)
+// { apple: 3, banana: 2, orange: 1 }
+console.log(count)
+
+let result = Object.keys(count).reduce((max, curr) => {
+    return count[curr] > count[max] ? curr : max;
+});
+
+console.log(result); // apple
